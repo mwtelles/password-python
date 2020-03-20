0c069ea8dcc57e230499c471fdee25c786bc9d1a
Matheus Telles <mwtelles2012@hotmail.com>
2020-03-20 15:10:05 -0300

    up to date

---
+++ passwords.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+MASTER_PASSWORD = "123456"
+
+senha = input("Insira sua senha master: ")
+if senha != MASTER_PASSWORD:
+    print("Senha invalida! Encerrando ...")
+    exit()
+
+conn = sqlite3.connect('password.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    service TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+''')
+
+def menu():
+    print('**********')
+    print('* i : Inserir nova senha')
+    print('* l : listar serviços salvos')
+    print('* r : recuperar uma senha')
+    print('* s : sair ')
+    print('**********')
+
+def get_password(service):
+    cursor.execute(f''' 
+        SELECT username, password FROM  users
+        WHERE service = '{service}''
+    ''')
+
+    if cursor.rowcount == 0:
+        print('Serviço não cadastrado (use l para verificar os serviços)')
+    else:
+        for user in cursor.fetchall():
+            print(user)
+
+def insert_password(service, username, password):
+    cursor.execute(f'''
+        INSERT INTO users (service, username, password)
+        VALUES ('{service}', '{username}', '{password}')
+    ''')
+    conn.commit()
+
+def show_services():
+    cursor.execute('''
+        SELECT service FROM users; 
+''')
+    for service in cursor.fetchall():
+        print(service)
+
+while True:
+    menu()
+    op = input('O que deseja fazer? ')
+    if op not in ['l', 'i', 'r', 's']:
+        print("Opção invalida!")
+        continue
+    
+    if op == 's':
+        break
+
+    if op == 'i':
+        service = input('Qual o nome do serviço? ')
+        username = input('Qual o nome de usuario? ')
+        password = input('Qual a senha? ')
+        insert_password(service, username, password)
+
+    if op == 'l':
+        show_services()
+
+    if op == 'r':
+        service = input('Qual serviço para o qual quer a senha recuperada ?')
+        get_password(service)
+
+conn.close()
+
+
